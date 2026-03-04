50dd30090dc4602ca9a2644c408fe4f9af5ab157
moshiurkn <rahaman2305341338@diu.edu.bd>
2026-03-04 22:58:51 +0600

    this feature was made by Moshiur (Step 1: Secure Admin Routes with Middleware)

--- routes/web.php
+++ routes/web.php
@@ -17,41 +17,32 @@ use App\Http\Controllers\ProfileController;
  */
 Route::get('/', [HomeController::class , 'Home'])->name('home');
 
-Route::get('logout', [AdminController::class , 'logout'])->name('logout');
-
-Route::get('/add_food', [AdminController::class , 'add_food'])->name('add_food');
-
-Route::get('/view_food', [AdminController::class , 'view_food'])->name('view_food');
-
-Route::post('/upload_food', [AdminController::class , 'upload_food'])->name('upload_food');
-
-Route::get('/update_food/{id}', [AdminController::class , 'update_food'])->name('update_food');
-
-Route::get('/delete_food/{id}', [AdminController::class , 'delete_food'])->name('delete_food');
-
-Route::post('/edit_food/{id}', [AdminController::class , 'edit_food'])->name('edit_food');
+Route::middleware(['auth', 'admin'])->group(function () {
+    Route::get('logout', [AdminController::class , 'logout'])->name('logout');
+    Route::get('/add_food', [AdminController::class , 'add_food'])->name('add_food');
+    Route::get('/view_food', [AdminController::class , 'view_food'])->name('view_food');
+    Route::post('/upload_food', [AdminController::class , 'upload_food'])->name('upload_food');
+    Route::get('/update_food/{id}', [AdminController::class , 'update_food'])->name('update_food');
+    Route::get('/delete_food/{id}', [AdminController::class , 'delete_food'])->name('delete_food');
+    Route::post('/edit_food/{id}', [AdminController::class , 'edit_food'])->name('edit_food');
 
 
 
-Route::post('/add_cart/{id}', [HomeController::class , 'add_cart'])->name('add_cart');
+    Route::post('/add_cart/{id}', [HomeController::class , 'add_cart'])->name('add_cart');
 
-Route::get('/my_cart', [HomeController::class , 'my_cart'])->name('my_cart');
+    Route::get('/my_cart', [HomeController::class , 'my_cart'])->name('my_cart');
 
-Route::get('/remove_cart/{id}', [HomeController::class , 'remove_cart'])->name('remove_cart');
+    Route::get('/remove_cart/{id}', [HomeController::class , 'remove_cart'])->name('remove_cart');
 
-Route::post('/confirm_order', [HomeController::class , 'confirm_order'])->name('confirm_order');
+    Route::post('/confirm_order', [HomeController::class , 'confirm_order'])->name('confirm_order');
+    Route::post('/book_table', [HomeController::class , 'book_table'])->name('book_table');
 
-Route::get('/orders', [AdminController::class , 'orders'])->name('orders');
-
-Route::get('/on_the_way/{id}', [AdminController::class , 'on_the_way'])->name('on_the_way');
-
-Route::get('/delivered/{id}', [AdminController::class , 'delivered'])->name('delivered');
-
-Route::get('/canceled/{id}', [AdminController::class , 'canceled'])->name('canceled');
-
-Route::post('/book_table', [HomeController::class , 'book_table'])->name('book_table');
-
-Route::get('/reservations', [AdminController::class , 'reservations'])->name('reservations');
+    Route::get('/orders', [AdminController::class , 'orders'])->name('orders');
+    Route::get('/on_the_way/{id}', [AdminController::class , 'on_the_way'])->name('on_the_way');
+    Route::get('/delivered/{id}', [AdminController::class , 'delivered'])->name('delivered');
+    Route::get('/canceled/{id}', [AdminController::class , 'canceled'])->name('canceled');
+    Route::get('/reservations', [AdminController::class , 'reservations'])->name('reservations');
+});
 
 
 
